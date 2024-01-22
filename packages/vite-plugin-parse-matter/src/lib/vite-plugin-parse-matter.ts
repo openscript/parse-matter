e05d1b652ff2f258f064b0b8181640a1915a5fda
Robin Bühler <git+r@obin.ch>
2024-01-22 07:42:04 +0000

    chore: initialize virtual module plugin

--- packages/vite-plugin-parse-matter/src/lib/vite-plugin-parse-matter.ts
+++ packages/vite-plugin-parse-matter/src/lib/vite-plugin-parse-matter.ts
@@ -1,3 +1,20 @@
-export function vitePluginParseMatter(): string {
-  return 'vite-plugin-parse-matter';
+export function vitePluginParseMatter() {
+  const virtualModuleId = 'virtual:parse-matter';
+  const resolvedVirtualModuleId = '\0' + virtualModuleId;
+
+  return {
+    name: 'parse-matter',
+    resolveId(id: string) {
+      if (id === virtualModuleId) {
+        return resolvedVirtualModuleId;
+      }
+      return undefined;
+    },
+    load(id: string) {
+      if (id === resolvedVirtualModuleId) {
+        return `export const msg = "from virtual module"`;
+      }
+      return undefined;
+    },
+  };
 }
