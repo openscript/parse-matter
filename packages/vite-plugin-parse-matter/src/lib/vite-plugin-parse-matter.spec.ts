1c34f884914eed2aa4c06813e978b8314106b36a
Robin Bühler <git+r@obin.ch>
2024-01-25 15:39:58 +0000

    chore: disable vite plugin tests

--- packages/vite-plugin-parse-matter/src/lib/vite-plugin-parse-matter.spec.ts
+++ packages/vite-plugin-parse-matter/src/lib/vite-plugin-parse-matter.spec.ts
@@ -1,7 +1,6 @@
-// import { vitePluginParseMatter } from './vite-plugin-parse-matter';
-//
-// describe('vitePluginParseMatter', () => {
-//   it('should work', () => {
-//     expect(vitePluginParseMatter()).toEqual('vite-plugin-parse-matter');
-//   });
-// });
+import { vitePluginParseMatter } from './vite-plugin-parse-matter';
+describe('vitePluginParseMatter', () => {
+  it('should work', () => {
+    expect(vitePluginParseMatter()).toMatchSnapshot()
+  });
+});
